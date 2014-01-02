38acb63ad055cf314f90dbb17e6e9e15692a0032
urtziperez <urtziperez_257@hotmail.com>
2014-01-02 16:27:09 +0100

    supermarkerv2

--- supermarker/settings.py
+++ supermarker/settings.py
@@ -95,6 +95,3 @@ PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'staticfiles'),
-)
