3ef3c18026aee600d34d4992de32404ab53a75f5
urtziperez <urtziperez_257@hotmail.com>
2014-01-02 16:31:41 +0100

    supermarkerv2

--- supermarker/urls.py
+++ supermarker/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+import settings
 #from django.contrib import admin
 admin.autodiscover()
 
