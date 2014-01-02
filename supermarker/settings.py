085013a3eddc3bcc5a949c6e3191bcef25d36d65
urtziperez <urtziperez_257@hotmail.com>
2014-01-02 17:13:39 +0100

    supermarkerv2

--- supermarker/settings.py
+++ supermarker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'd@=cu152h_lj236ckzrm#u$+b(lu2%wo8v$*$+@j3bqbegjn6a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
